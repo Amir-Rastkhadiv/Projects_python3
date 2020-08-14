6613fdc51a444363c545a30b3efe90ab88ea7025
Amir Rastkhadivmasouleh <amir.rastkhadiv@gmail.com>
2020-08-14 17:26:20 +0100

    Added test objects

--- Bordless_Tourist.py
+++ Bordless_Tourist.py
@@ -2,3 +2,5 @@ destinations = ["Paris, France",
 "Shanghai, China", "Los Angeles, USA", 
 "São Paulo, Brazi", "Cairo, Egyp"]
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+print()
